f5e9a21924d011483f0ebb3f4190403fea5791d6
Malthe Petersen <malthe@maggerit.dk>
2017-03-20 17:15:37 +0100

    Created the homepage when loggedin - Malthe

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
-@section('headlerLeft')
-	Home
+@section('headerLeft')
+	<i class="fa fa-arrow-left" aria-hidden="true"></i>
 @endsection
 
 @section('headline')
@@ -9,5 +9,90 @@
 @endsection
 
 @section('headerRight')
-	Home
+	<i class="fa fa-pencil" aria-hidden="true"></i>
+@endsection
+
+@section('content')
+<main id="withMargin">
+	@include('partials.search')
+
+	<section class="addNew">
+		<div>
+			<i class="fa fa-plus-circle" aria-hidden="true"></i>
+			<p>Client</p>
+		</div>
+		<div>
+			<i class="fa fa-plus-circle" aria-hidden="true"></i>
+			<p>Pump</p>
+		</div>
+	</section>
+
+
+	<section class="searchResultCategory">
+		<h2>Clients</h2>
+		<article>
+			<div>
+				<img src="images/client.png">
+			</div>
+			<div>
+				<h3>Erhvervsakademi Aarhus</h3>
+				<p>Sønderhøj 30, 8260 Viby J</p>
+			</div>
+		</article>
+				<article>
+			<div>
+				<img src="images/client.png">
+			</div>
+			<div>
+				<h3>Erhvervsakademi Aarhus</h3>
+				<p>Sønderhøj 30, 8260 Viby J</p>
+			</div>
+		</article>
+				<article>
+			<div>
+				<img src="images/client.png">
+			</div>
+			<div>
+				<h3>Erhvervsakademi Aarhus</h3>
+				<p>Sønderhøj 30, 8260 Viby J</p>
+			</div>
+		</article>
+	</section>
+
+	<section class="searchResultCategory">
+		<h2>Pumps</h2>
+		<article>
+			<div>
+				<img src="images/alpha3.jpg">
+			</div>
+			<div>
+				<h3>Alpha 3 in basement</h3>
+				<p>APHA3 15-80 130</p>
+				<p class="location">Ringvejsyd 104, 8260 Viby J</p>
+			</div>
+		
+		</article>
+		<article>
+			<div>
+				<img src="images/alpha3.jpg">
+			</div>
+			<div>
+				<h3>Alpha 3 in basement</h3>
+				<p>APHA3 15-80 130</p>
+				<p class="location">Ringvejsyd 104, 8260 Viby J</p>
+			</div>
+		</article>
+		<article>
+			<div>
+				<img src="images/alpha3.jpg">
+			</div>
+			<div>
+				<h3>Alpha 3 in basement</h3>
+				<p>APHA3 15-80 130</p>
+				<p class="location">Ringvejsyd 104, 8260 Viby J</p>
+			</div>
+		</article>
+	</section>
+</main>
+
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -27,7 +27,9 @@
         <section>
             @yield('headerLeft')
         </section>
-        <h1>@yield('headline')</h1>
+        <section>
+            <h1>@yield('headline')</h1>
+        </section>
         <section>
             @yield('headerRight')
         </section>
@@ -35,6 +37,36 @@
 
 
         @yield('content')
+
+
+    <nav class="bottomNav">
+        <ul>
+            <li>
+                <a href="#">
+                    <i class="fa fa-home" aria-hidden="true"></i>
+                    <p>Home</p>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <i class="fa fa-folder" aria-hidden="true"></i>
+                    <p>Catalogue</p>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <i class="fa fa-user" aria-hidden="true"></i>
+                    <p>Profile</p>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <i class="fa fa-home" aria-hidden="true"></i>
+                    <p>Grundfos</p>
+                </a>
+            </li>
+        </ul>
+    </nav>
 
 </div>
     <!-- Scripts -->
