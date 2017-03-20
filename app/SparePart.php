6e90368681e16f20bdcd36ba409da264f7f8a325
Pederytter <pederytter@gmail.com>
2017-03-20 15:30:20 +0100

    Login

--- app/SparePart.php
+++ app/SparePart.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class PumpTools extends Model
+class SparePart extends Model
 {
     //
 }
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,6 +1,61 @@
 @extends('layouts.app')
 
 @section('content')
+<main id="register">
+    <form action="">
+        <img src="images/Grundfos_Logo.png" alt="">
+        {{ csrf_field() }}
+
+        <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
+
+            <div class="col-md-6">
+                <input id="name" type="text" class="form-control" placeholder="Full name" name="name" value="{{ old('name') }}" required autofocus>
+
+                @if ($errors->has('name'))
+                <span class="help-block">
+                    <strong>{{ $errors->first('name') }}</strong>
+                </span>
+                @endif
+            </div>
+        </div>
+
+        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+
+            <div class="col-md-6">
+                <input id="email" type="email" placeholder="Email" class="form-control" name="email" value="{{ old('email') }}" required>
+
+                @if ($errors->has('email'))
+                <span class="help-block">
+                    <strong>{{ $errors->first('email') }}</strong>
+                </span>
+                @endif
+            </div>
+        </div>
+
+        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+
+            <div class="col-md-6">
+                <input id="password" type="password" placeholder="Password" class="form-control" name="password" required>
+
+                @if ($errors->has('password'))
+                <span class="help-block">
+                    <strong>{{ $errors->first('password') }}</strong>
+                </span>
+                @endif
+            </div>
+        </div>
+
+        <div class="form-group">
+            <div class="col-md-6 col-md-offset-4">
+                <button type="submit" class="btn btn-primary">
+                    Register
+                </button>
+            </div>
+        </div>
+    </form>
+</main>
+
+<!--
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
@@ -73,4 +128,5 @@
         </div>
     </div>
 </div>
+-->
 @endsection
